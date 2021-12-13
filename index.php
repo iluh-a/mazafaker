2a6677e831f94f067b6f9b0019dd546f40132510
iluh-a <iluhuserbayev@gmail.com>
2021-12-13 16:43:25 +0600

    asdf

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ $chat_id = $data['message']['chat']['id'];
 $text = 'salamaleykum';
 
 $bot_token = "5002383329:AAGfbr-RTuvNP1R_PnVs19_WRXFXsTdDDQk";
-file_get_contents("https://api.telegram.org/bot$bot_token/sendMessage?chat_id=1341766485&text=$data");
+file_get_contents("https://api.telegram.org/bot$bot_token/sendMessage?chat_id=1341766485&text=$input");
