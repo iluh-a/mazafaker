37b0aa71dacec0a4c038cae004920501939ea87d
iluh-a <iluhuserbayev@gmail.com>
2021-12-13 17:53:56 +0600

    asdf

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ $text = $data['message']['text'];
 $api_key = '108856a2254a8616fc93091eb1c18d9f';
 
 $response = file_get_contents("https://api.themoviedb.org/3/search/movie?api_key=$api_key&language=en-US&query=$text&page=1&include_adult=false");
-//$message = $response['results'];
-file_get_contents("https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=salsa");
+$message = $response['results'];
+file_get_contents("https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$message");
