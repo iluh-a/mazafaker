81c647380692633fb32d7925f2a8aa5f86ac79d4
iluh-a <iluhuserbayev@gmail.com>
2021-12-14 11:13:33 +0600

    asdf

--- index.php
+++ index.php
@@ -7,9 +7,8 @@ $text = $data['message']['text'];
 $api_key = '6a6dc0a3';
 
 $response = file_get_contents("http://www.omdbapi.com/?apikey=$api_key&s=$text");
-$obj = json_decode($response, true);
-$movie = $obj['Search'][0];
-$movie = urlencode($movie);
+$obj = json_decode($response);
+$movie = $obj->{'Search'};
 
 //$title = $movie['Title'];
 //$year = $movie['Year'];
