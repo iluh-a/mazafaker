731141be993f771c7484ead68a6b3d1f7c72fdf0
iluh-a <iluhuserbayev@gmail.com>
2021-12-14 10:44:57 +0600

    asdf

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ $text = $data['message']['text'];
 $api_key = '6a6dc0a3';
 
 $response = file_get_contents("http://www.omdbapi.com/?apikey=$api_key&s=$text");
-//$movie = $response['Search'];
+$movie = $response['Search'];
 //$title = $movie['Title'];
 //$year = $movie['Year'];
 //$poster = $movie['Poster'];
 //$message = "$title, $year, $poster";
-file_get_contents("https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$response");
+file_get_contents("https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$movie");
