68c8488d5b6d6fca2fd12001986a503f38e75f50
iluh-a <iluhuserbayev@gmail.com>
2021-12-14 10:12:34 +0600

    asdf

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ $chat_id = $data['message']['chat']['id'];
 $text = $data['message']['text'];
 $api_key = '6a6dc0a3';
 
-$response = file_get_contents("http://www.omdbapi.com/?apikey=$api_key&t=$text");
-$message = [
-    'title' => $response['Title'],
-    'language' => $response['Language'],
-    'genre' => $response['Genre'],
-    'year' => $response['Year'],
-    'imdbRating' => $response['imdbRating']
-];
-file_get_contents("https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$message");
+$response = file_get_contents("http://www.omdbapi.com/?apikey=$api_key&s=$text");
+//$message = [
+//    'title' => $response['Title'],
+//    'language' => $response['Language'],
+//    'genre' => $response['Genre'],
+//    'year' => $response['Year'],
+//    'imdbRating' => $response['imdbRating']
+//];
+file_get_contents("https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$response");
